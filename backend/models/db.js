9bbc200ca3a5c4b793f043d723254168eaab9091
sam khe <visuelkoncept@gmail.com>
2021-12-03 06:20:59 +0100

    projetTermine

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,14 @@
 const { Sequelize, Models, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-});
-
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
 
 module.exports = sequelize;
